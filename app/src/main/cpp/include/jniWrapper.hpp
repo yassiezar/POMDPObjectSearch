d2627af85009cf259559a4f6b86525b4017631ff
Jaycee Lock <jaycee.lock@gmail.com>
2018-09-04 09:04:29 +0100

    Removed old/useless codde

--- app/src/main/cpp/include/jniWrapper.hpp
+++ app/src/main/cpp/include/jniWrapper.hpp
@@ -3,15 +3,11 @@
 
 #include <jni.h>
 #include <android/log.h>
-#include <MDP/mdp.hpp>
 #include <SoundGenerator/SoundGenerator.hpp>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-JNIEXPORT bool JNICALL Java_com_example_jaycee_pomdpobjectsearch_JNIBridge_initSearch(JNIEnv*, jobject, jlong, jlong);
-JNIEXPORT jlong JNICALL Java_com_example_jaycee_pomdpobjectsearch_JNIBridge_getAction(JNIEnv*, jobject, jlong);
 
 JNIEXPORT bool JNICALL Java_com_example_jaycee_pomdpobjectsearch_JNIBridge_initSound(JNIEnv*, jobject);
 JNIEXPORT bool JNICALL Java_com_example_jaycee_pomdpobjectsearch_JNIBridge_killSound(JNIEnv*, jobject);
@@ -21,7 +17,6 @@ JNIEXPORT void JNICALL_Java_com_example_jaycee_pomdpobjectsearch_JNIBridge_playS
 }
 #endif
 
-static MDPNameSpace::MDP mdp;
 static SoundGenerator::SoundGenerator soundGenerator;
 
 #endif
--- app/src/main/cpp/src/jniWrapper.cpp
+++ app/src/main/cpp/src/jniWrapper.cpp
@@ -5,21 +5,6 @@ extern "C" {
 #endif
 
 JNIEXPORT bool JNICALL
-Java_com_example_jaycee_pomdpobjectsearch_JNIBridge_initSearch(JNIEnv* env, jobject obj, jlong target, jlong horizon)
-{
-    mdp.setTarget(target);
-    mdp.solve(horizon);
-
-    return true;
-}
-
-JNIEXPORT jlong JNICALL
-Java_com_example_jaycee_pomdpobjectsearch_JNIBridge_getAction(JNIEnv* env, jobject obj, jlong state)
-{
-    mdp.getAction(state);
-}
-
-JNIEXPORT bool JNICALL
 Java_com_example_jaycee_pomdpobjectsearch_JNIBridge_initSound(JNIEnv* env, jobject obj)
 {
     soundGenerator.init();
